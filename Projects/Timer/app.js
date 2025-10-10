7ee320c0e4bae315bb332f9aa26cc3a612a0d9b1
João Pedro Telles Fava <88334985+jaypeter19@users.noreply.github.com>
2025-10-10 18:23:24 -0300

    timer project class done banckAccount exercise

---
+++ Projects/Timer/app.js
@@ -0,0 +1,17 @@
+const durantionInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durantionInput, startButton, pauseButton, {
+    onStart() {
+        console.log('Timer Started')
+    },
+    onTick() {
+        console.log("Timer just ticked down")
+    },
+    onComplete() {
+        console.log("Timer is completed")
+    }
+});
+
+
---
+++ Projects/Timer/timer.js
@@ -0,0 +1,49 @@
+class Timer {
+    constructor(durantionInput, startButton, pauseButton, callbacks) {
+        this.durantionInput = durantionInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.ontick;
+            this.onComplete = callbacks.onComplete;
+        }
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        if (this.onStart) {
+            this.onStart();
+        }
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+    };
+
+    pause = () => {
+        clearInterval(this.interval);
+    };
+
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
+        } else {
+            this.timeRemaining = this.timeRemaining - 1;
+            if (this.onTick) {
+                this.onTick();
+            }
+        }
+    };
+
+    get timeRemaining() {
+        return parseFloat(this.durantionInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durantionInput.value = time;
+    }
+}
