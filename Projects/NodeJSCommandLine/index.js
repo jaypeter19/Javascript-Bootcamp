3ba83183301aadf64f44b6a6be5c2223a7ceb97c
João Pedro Telles Fava <88334985+jaypeter19@users.noreply.github.com>
2025-10-16 13:19:58 -0300

    Node JS Command Line project first commit

---
+++ Projects/NodeJSCommandLine/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+fs.readdir(process.cwd(), (err, filenames) => {
+
+    if (err) {
+        console.log(err)
+        throw new Error(err);
+    }
+
+    console.log(filenames)
+});
