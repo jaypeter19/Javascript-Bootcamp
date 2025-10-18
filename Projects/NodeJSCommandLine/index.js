a1e445828654cf402a3909a8ca7ea7de28ea369a
João Pedro Telles Fava <88334985+jaypeter19@users.noreply.github.com>
2025-10-18 10:09:15 -0300

    Node JS command line partial commit

--- Projects/NodeJSCommandLine/index.js
+++ Projects/NodeJSCommandLine/index.js
@@ -3,11 +3,31 @@
 const fs = require('fs');
 
 fs.readdir(process.cwd(), (err, filenames) => {
+  if (err) {
+    console.log(err);
+  }
 
-    if (err) {
-        console.log(err)
-        throw new Error(err);
-    }
+  const allStats = Array(filenames.length).fill(null);
 
-    console.log(filenames)
+  for (let filename of filenames) {
+    const index = filenames.indexOf(filename);
+
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+
+      allStats[index] = stats;
+
+      const ready = allStats.every(stats => {
+        return stats;
+      });
+
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(filenames[index], stats.isFile());
+        });
+      }
+    });
+  }
 });
