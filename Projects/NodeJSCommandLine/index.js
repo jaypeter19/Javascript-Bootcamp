54a8c65cb36ab1dbb94228b0729fdb86a2444c10
João Pedro Telles Fava <88334985+jaypeter19@users.noreply.github.com>
2025-10-28 18:36:26 -0300

    first bad approach commit

--- Projects/NodeJSCommandLine/index.js
+++ Projects/NodeJSCommandLine/index.js
@@ -4,30 +4,17 @@ const fs = require('fs');
 
 fs.readdir(process.cwd(), (err, filenames) => {
   if (err) {
-    console.log(err);
+    throw new Error(err)
   }
 
-  const allStats = Array(filenames.length).fill(null);
-
-  for (let filename of filenames) {
-    const index = filenames.indexOf(filename);
-
+  //BAD APPROACH 
+  for (const filename of filenames) {
     fs.lstat(filename, (err, stats) => {
       if (err) {
-        console.log(err);
+        console.log(err)
       }
-
-      allStats[index] = stats;
-
-      const ready = allStats.every(stats => {
-        return stats;
-      });
-
-      if (ready) {
-        allStats.forEach((stats, index) => {
-          console.log(filenames[index], stats.isFile());
-        });
-      }
-    });
+      console.log(filename, stats.isFile())
+    })
   }
+
 });
