607c34d50eb3a6ba8e939c8c9be89afacdcef842
João Pedro Telles Fava <88334985+jaypeter19@users.noreply.github.com>
2025-10-28 10:29:00 -0300

    basic world setup engine, maze generation logic added

--- Projects/MazeGame/app.js
+++ Projects/MazeGame/app.js
@@ -3,12 +3,17 @@ const { Engine, Render, Runner, World, Bodies } = Matter;
 const engine = Engine.create();
 const { world } = engine;
 
+const cells = 3;
+const width = 600;
+const height = 600;
+
 const render = Render.create({
     element: document.body,
     engine: engine,
-    option: {
-        width: 800,
-        height: 600
+    options: {
+        wireframes: true,
+        width: width,
+        height: height
     }
 });
 
@@ -16,8 +21,38 @@ Render.run(render);
 Runner.run(Runner.create(), engine);
 
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-    isStatic: true
+// Walls
+const walls = [
+    Bodies.rectangle(width / 2, 0, width, 40, {
+        isStatic: true
+    }),
+    Bodies.rectangle(width / 2, height, width, 40, {
+        isStatic: true
+    }),
+    Bodies.rectangle(0, height / 2, 40, height, {
+        isStatic: true
+    }),
+    Bodies.rectangle(width, height / 2, 40, height, {
+        isStatic: true
+    })
+];
+
+World.add(world, walls);
+
+
+// Maze Generation 
+
+
+const grid = Array(cells).fill(null).map(() => {
+    return Array(cells).fill(false);
 });
 
-World.add(world, shape);
+const verticals = Array(cells).fill(null).map(() => {
+    return Array(cells - 1).fill(false);
+})
+
+const horizontals = Array(cells - 1).fill(null).map(() => {
+    return Array(cells).fill(false);
+})
+
+
