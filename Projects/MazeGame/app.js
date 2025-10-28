51859fc6674f7aef52f12d0c05b3a24b31a7af80
João Pedro Telles Fava <88334985+jaypeter19@users.noreply.github.com>
2025-10-28 15:35:40 -0300

    maze generation logic

--- Projects/MazeGame/app.js
+++ Projects/MazeGame/app.js
@@ -1,19 +1,21 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const engine = Engine.create();
-const { world } = engine;
-
 const cells = 3;
 const width = 600;
 const height = 600;
+const unitLength = width / cells;
+
+const engine = Engine.create();
+const { world } = engine;
+
 
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
         wireframes: true,
-        width: width,
-        height: height
+        width,
+        height
     }
 });
 
@@ -42,6 +44,22 @@ World.add(world, walls);
 
 // Maze Generation 
 
+const shuffle = (arr) => {
+    let counter = arr.length;
+
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+
+        counter--;
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+    }
+
+    return arr;
+}
+
 
 const grid = Array(cells).fill(null).map(() => {
     return Array(cells).fill(false);
@@ -56,3 +74,88 @@ const horizontals = Array(cells - 1).fill(null).map(() => {
 })
 
 
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+
+const currentCell = (row, column) => {
+
+    if (grid[row][column]) {
+        return;
+    }
+
+    grid[row][column] = true;
+
+    const neighbors = shuffle([
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left']
+    ]);
+
+    for (let neighbor of neighbors) {
+        const [nextRow, nextColumn, direction] = neighbor;
+
+        if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+            continue;
+        }
+
+        if (grid[nextRow][nextColumn]) {
+            continue;
+        }
+
+        if (direction === 'left') {
+            verticals[row][column - 1] = true;
+        } else if (direction === 'right') {
+            verticals[row][column] = true;
+        } else if (direction === 'up') {
+            horizontals[row - 1][column] = true;
+        } else if(direction === 'down') {
+            horizontals[row][column] = true;
+        }
+
+        currentCell(nextRow,  nextColumn);
+    }
+
+};
+
+
+currentCell(startRow, startColumn);
+
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength / 2,
+            rowIndex * unitLength + unitLength,
+            unitLength,
+            5,
+            {
+                isStatic: true
+            }
+        );
+        World.add(world, wall)
+    });
+});
+
+
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength / 2,
+            5,
+            unitLength,
+            {
+                isStatic: true
+            }
+        );
+        World.add(world, wall)
+    })
+})
