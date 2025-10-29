b5406c42182d556347269da4d46b70e9ed8c78af
João Pedro Telles Fava <88334985+jaypeter19@users.noreply.github.com>
2025-10-29 17:26:00 -0300

    validation signup logic added

--- Projects/E-CommerceApp/index.js
+++ Projects/E-CommerceApp/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const usersRepo = require('./repositories/users');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
@@ -12,14 +14,26 @@ app.get('/', (req, res) => {
         <form method="POST">
         <input type="email" name="email">
         <input type="password" name="password">
+        <input type="password" name="passwordConfirmation">
         <button>Sign Up</button>
         </form>
         </div>
     `)
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body)
+app.post('/', async (req, res) => {
+    const { email, password, passwordConfirmation } = req.body;
+
+    const existingUser  = await usersRepo.getOneBy({email: email});
+
+    if (existingUser) {
+        return res.send('Email already registered.')
+    }
+
+    if (password !== passwordConfirmation) {
+        return res.send('Password must match. Please type again.')
+    }
+
     res.send('Account created!');
 })
 
