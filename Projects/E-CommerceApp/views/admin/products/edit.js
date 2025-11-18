a9a662d32ad4ae56efafbcab9d502c724b798df0
João Pedro Telles Fava <88334985+jaypeter19@users.noreply.github.com>
2025-11-17 22:28:03 -0300

    gitignore slides directory added

---
+++ Projects/E-CommerceApp/views/admin/products/edit.js
@@ -0,0 +1,27 @@
+const layout = require('../layout');
+
+module.exports = ({product}) => {
+    return layout({
+        content: 
+        `
+        <h2 class="mb-3">Edit Product</h2>
+        <form method="POST" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" name="title" id="title" class="form-control" placeholder="Title" value="${product.title}">
+                <div id="titleHelp" class="form-text">${getError(errors, 'title')}</div>
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Price</label>
+                <input type="number" name="price" id="price" class="form-control" placeholder="Price" value="${product.price}">
+                <div id="priceHelp" class="form-text">${getError(errors, 'price')}</div>
+            </div>
+            <div class="mb-3">
+                <label for="image" class="form-label">Image</label>
+                <input type="file" name="image" id="image" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Edit</button>
+        </form>
+        `
+    })
+}
